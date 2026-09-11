6fb34cb35b5c06eb34157749063e91d6936fdc0b
Akshay-dot123 <akshaytest234@gmail.com>
2026-09-11 16:25:07 +0530

    Fix Navbar casing

--- bookstore-client/src/App.jsx
+++ bookstore-client/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Navbar from "./components/Navbar.jsx";
+import Navbar from "./components/NavBar.jsx";
 import ProtectedRoute from "./components/ProtectedRoute";
 
 import Home from "./pages/Home";
